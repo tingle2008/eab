0169a3057a3665e29e1aa4f3386fab347075e792
tingle2008 <tingle2008@gmail.com>
2017-04-07 13:09:21 -0700

    xxx

--- forexsys/accounts/models.py
+++ forexsys/accounts/models.py
@@ -1,17 +1,5 @@
+from __future__ import unicode_literals
+
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    force_change_password = models.BooleanField()
-
-
-def create_user_profile(sender, instance, created, **kwargs):
-    # refer to https://docs.djangoproject.com/en/dev/topics/auth/#django.contrib.auth.models.UserManager.create_user
-    if created:
-        UserProfile.objects.create(user=instance)
-
-
-post_save.connect(create_user_profile, sender=User)
+# Create your models here.
